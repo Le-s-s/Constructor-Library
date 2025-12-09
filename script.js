c96c33ee762f43c47c411a6ba4d967a0d5d14421
Le-s-s <lexton.d.lewis1@gmail.com>
2025-12-09 11:46:39 -0700

    Can add books through add to library function.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 let library = [];
-function book (author,title,pages,read){
-    if(!new.target){
-        throw Error("This is a constructor.")
-    }
+
+
+
+function book(author, title, pages, read) {
+    if (!new.target) throw new Error("This is a constructor.");
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
-    this.info = `${this.title} by ${this.author}, it's ${this.pages} pages.`;
+    this.info = `${title} by ${author}, it's ${pages} pages.`;
     this.id = crypto.randomUUID();
-};
+}
 
-function addToLibrary (author,title,pages) {
-    const bookToAdd = new book(author,title,pages);
+function addToLibrary(author, title, pages) {
+    let bookToAdd = new book(author, title, pages);
+    let viewLibrary = document.querySelector(".grid");
+    let viewBook = document.createElement("div");
     library.push(bookToAdd);
-};
+    viewBook.classList.add("card");
+    viewBook.textContent = `${bookToAdd.title}`;
+    viewBook.id = bookToAdd.id;
+    viewLibrary.appendChild(viewBook);
+}
 
-function inLibrary () {
+function inLibrary() {
     library.forEach(element => {
         alert(element.info);
         alert(element.id);
     });
-}
+}   
+
+//todo
+//add ability to let people add books using a button
